4732d1a02e3c8772b5d9c267fa6e9ea15aea558b
Taylor Cavaletto <tjcavaletto@gmail.com>
2025-06-13 14:42:21 -0400

    - Previous fix was broken

--- com.bemaservices.RoomManagement/Plugins/com_bemaservices/RoomManagement/ReservationList.ascx.cs
+++ com.bemaservices.RoomManagement/Plugins/com_bemaservices/RoomManagement/ReservationList.ascx.cs
@@ -490,7 +490,7 @@ namespace RockWeb.Plugins.com_bemaservices.RoomManagement
             }
             reservationQueryOptions.LocationIds = locationIdList;
 
-            reservationQueryOptions.CampusIds = cpCampuses.SelectedValuesAsInt().ToList();
+            reservationQueryOptions.CampusIds = cpCampuses.SelectedCampusIds;
 
             var qry = reservationService.Queryable( reservationQueryOptions );
 
